6f9770fb88e782c85de5626d8c3383c2e3b6e35c
Stephan Goertz <stephan.goertz@gmail.com>
2024-12-12 11:29:04 +0100

    chapter 3

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -18,4 +19,27 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+type envelope map[string]any
+
+// Sends a JSON response with a HTTP status code, some data and a header map containing
+// any additional HTTP headers we want to include.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	j, err := json.Marshal(data) // json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	j = append(j, '\n') // to make it easier to view in terminal apps
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(j)
+
+	return nil
 }
